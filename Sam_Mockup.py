06bea5d4ba7788885761c22ca5fcf2b0e6aef25b
Zachary Werner <85905928+Zachjaryw@users.noreply.github.com>
2023-02-13 16:28:57 -0800

    Update Sam_Mockup.py

--- Sam_Mockup.py
+++ Sam_Mockup.py
@@ -7,7 +7,7 @@ dropdowns = {'Drop1':'Text',
               'Drop2':'Text',
                'Drop3':'Text'}
                
-keys = dropdowns.keys().tolist()
+keys = dropdowns.keys.tolist()
 for dropdown in range(len(dropdowns.keys())):
   with st.container(keys[dropdown]):
     st.write(dropdowns[keys[dropdown]])
